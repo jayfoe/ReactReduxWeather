8f51b48a2634ae304a041311d062fa7d22f5c3f6
u509625 <Jonathan.Ford@wellsfargo.com>
2017-03-27 15:23:35 -0700

    Fetch weather reducer. Table header on page.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
-    var k = 'foo' + event.target;
-    console.log("value",event.target.value);
-    console.log("target",event.target);
-    console.log("event",event);
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input 
           placeholder="Get a five-day forecast"
           className="form-control"
           value={this.state.term}
-          onChange={this.onInputChange}
-        />
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             Submit
@@ -32,3 +40,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
